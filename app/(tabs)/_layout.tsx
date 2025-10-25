b37aa1c211ce70a2d9701324bbbaf8f2b93f73c2
betomoedano <betomoedano@outlook.com>
2025-10-25 17:43:48 -0500

    update deps

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,20 +1,24 @@
+import { Color } from "@/constants/TWPalette";
 import { Icon, Label, NativeTabs } from "expo-router/unstable-native-tabs";
 import React from "react";
 
 export default function TabLayout() {
   return (
-    <NativeTabs disableTransparentOnScrollEdge={true}>
+    <NativeTabs
+      disableTransparentOnScrollEdge={true}
+      tintColor={Color.slate[50]}
+    >
       <NativeTabs.Trigger name="home">
         <Label>Home</Label>
-        <Icon sf="house" />
+        <Icon sf="house.fill" />
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="tattoos">
         <Label>My Tattoos</Label>
-        <Icon sf="sprinkler.and.droplets" />
+        <Icon sf="photo.fill.on.rectangle.fill" />
       </NativeTabs.Trigger>
-      <NativeTabs.Trigger name="profile">
+      <NativeTabs.Trigger name="profile" role="search">
         <Label>Profile</Label>
-        <Icon sf="person" />
+        <Icon sf="person.fill" />
       </NativeTabs.Trigger>
     </NativeTabs>
   );
