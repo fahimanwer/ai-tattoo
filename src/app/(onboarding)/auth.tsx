04398424307ed18562992e78e655f3924f728e39
betomoedano <betomoedano@outlook.com>
2025-12-26 10:42:51 -0600

    Enhance onboarding authentication screen with background image and layout adjustments
    - Introduced a background image to the onboarding authentication screen, improving visual appeal.
    - Added a gradient overlay for better text readability against the background.
    - Adjusted layout to ensure proper spacing and alignment of the AuthContent component, enhancing user experience during account creation.

--- src/app/(onboarding)/auth.tsx
+++ src/app/(onboarding)/auth.tsx
@@ -1,7 +1,11 @@
 import { AuthContent } from "@/src/components/auth/AuthContent";
+import { Color } from "@/src/constants/TWPalette";
 import { AppSettingsContext } from "@/src/context/AppSettings";
+import { Image } from "expo-image";
 import { Stack } from "expo-router";
 import { use } from "react";
+import { StyleSheet, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { useUserData } from "../../hooks/useUserData";
 
 /**
@@ -12,6 +16,7 @@ import { useUserData } from "../../hooks/useUserData";
 export default function OnboardingAuth() {
   const { setIsOnboarded } = use(AppSettingsContext);
   const { refresh } = useUserData();
+  const { bottom } = useSafeAreaInsets();
 
   const handleSuccess = async () => {
     await refresh();
@@ -27,12 +32,35 @@ export default function OnboardingAuth() {
           headerShown: false,
         }}
       />
-      <AuthContent
-        title="One more step!"
-        description="Create an account to activate your subscription"
-        onSuccess={handleSuccess}
-        style={{ flex: 1, paddingTop: 60, backgroundColor: "#000" }}
-      />
+      {/* Background */}
+      <View style={StyleSheet.absoluteFill}>
+        <View
+          style={[
+            StyleSheet.absoluteFill,
+            {
+              zIndex: 1,
+              experimental_backgroundImage: `linear-gradient(to bottom, transparent 0%, ${Color.grayscale[50]} 50%)`,
+            },
+          ]}
+        />
+        <Image
+          source={{
+            uri: "https://d3ynb031qx3d1.cloudfront.net/ai-tattoo/demos/paywall-bg.avif",
+          }}
+          contentFit="cover"
+          contentPosition="center"
+          style={[StyleSheet.absoluteFill]}
+        />
+      </View>
+
+      <View style={{ flex: 1, justifyContent: "flex-end", zIndex: 2 }}>
+        <AuthContent
+          title="One more step!"
+          description="Create an account to activate your subscription. This helps us track your subscription and provide you with the best experience."
+          onSuccess={handleSuccess}
+          style={{ marginBottom: bottom }}
+        />
+      </View>
     </>
   );
 }
